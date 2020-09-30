3ad883bd9c6f5f97444536a967aad1b93f7b1eb0
Abdul <abdulahad2004@yahoo.com>
2020-09-30 21:05:32 +0300

    struggles

--- Year 11 Term 1/Email check/main.py
+++ Year 11 Term 1/Email check/main.py
@@ -1,41 +1,25 @@
 """
-ask user for 5 emails
-insert the emails into a text file, separated by a semi colon
+save all text of emails.txt as a list
 
+loop through the list of emails:
+    check if it is valid through a regex:
+        if not, ask the user to re-enter it and replace it on the list
+        else, continue to the next email
 
-for every email, check if it is valid
+after all emails have been checked:
+    re-write the text file with the new, correct email list
+
+custom functions: validate_email(email)
 """
 
+# import re
 
+# emails = list()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# def get_emails():
+#     emails_file = open(r"emails.txt", "r")
+#     emails = emails_file.readlines()
+#     print(emails)
 
 
 
@@ -45,28 +29,30 @@ emails = list()
 
 def get_emails():
   # get email from the user
-  while len(emails) < 5:
-    email = input("Enter email: ")
-    if validate_email(email):
-      append_email(email)
+    while len(emails) < 5:
+        email = input("Enter email: ")
+        if validate_email(email):
+            append_email(email)
+    return True
 
 def validate_email(emailToValidate):
     regex = r"^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$"
 
     if (re.search(regex, emailToValidate)):
-      return True
+        return True
     else:
         print(f"Incorrect email: {emailToValidate}")
         return False
 
 def append_email(emailToAppend):
-  emails.append(emailToAppend)
+    emails.append(emailToAppend)
 
 def export_emails(emailList):
-  file = open("emails.txt", "w")
-  for email in emailList:
-    file.write(email)
-
-  file.close()
+    emails_file = open("emails.txt", "a")
+    for email in emailList:
+        emails_file.write(email)
+    emails_file.close()
     
-get_emails()
+if get_emails():
+    export_emails(emails)
+    print(emails)
