ad44177b3a8262b9e045d14bd54a0a4098d9cd6e
Abdul <abdulahad2004@yahoo.com>
2020-09-30 21:29:14 +0300

    Email program

---
+++ Year-11-Term-1/email-program/main.py
@@ -0,0 +1,32 @@
+import re
+
+emails = list()
+
+def get_emails():
+  # get email from the user
+    while len(emails) < 5:
+        email = input("Enter email: ")
+        if validate_email(email):
+            append_email(email)
+    return True
+
+def validate_email(emailToValidate):
+    regex = r"^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$"
+
+    if (re.search(regex, emailToValidate)):
+        return True
+    else:
+        print(f"Incorrect email: {emailToValidate}")
+        return False
+
+def append_email(emailToAppend):
+    emails.append(emailToAppend)
+
+def export_emails(emailList):
+    emails_file = open(r"Year-11-Term-1/email-program/emails.txt", "a")
+    for email in emailList:
+        emails_file.write(f"{email}\n")
+    emails_file.close()
+    
+if get_emails():
+    export_emails(emails)
