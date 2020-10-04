eab064542ca01d287a6a58fa1f9f86f53df9fce6
Abdul <abdulahad2004@yahoo.com>
2020-10-04 20:29:37 +0300

    RLE Algorithm

--- Year-11-Term-1/Week 4/4.2.1.py
+++ Year-11-Term-1/Week 4/4.2.1.py
@@ -1,15 +1,41 @@
-# loop through each character = i
-# check every follwoing character until it != i
-#  
+"""
+This algorithm implements the RLE compression method to a string.
 
-string = "AAABBBBBCCC"
-count = 0
-index = 0
-j = string[0]
+Loop through each character starting with the first. 
+Count the occurences of this letter following it until a different character is found.
+Append the count and the letter to a separate string. 
 
-for letter in string:
-  letter = j
-  while j == letter:
-    if string[index+1] == letter:
-      count += 1
-      print(f"count {count}") 
+
+AAABBBBBBBCCC
+becomes 3A7B3C
+"""
+
+def rle_encode(string):
+
+  # i is the index of the letter being checked for 
+  # j is/are the following chars
+
+  encoded = ""
+  i = 0
+
+  while i < len(string):
+    curr_char = string[i]
+    count = 1
+    j = i
+
+    while j < len(string)-1:
+      if (string[j] == string[j+1]):
+        count += 1
+        j += 1
+      else: 
+        break
+    
+    encoded += str(count) + curr_char
+    i = j + 1
+
+  return encoded
+
+usr_input = input("String: ")
+encoded_string = rle_encode(usr_input)
+
+print(encoded_string.replace("1", ""))
