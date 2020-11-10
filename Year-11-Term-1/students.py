b247c36f043d7411f636cb490ae6bc0c72d9e13f
azure8117 <abdulahad2004@yahoo.com>
2020-11-10 18:40:23 +0300

    Students Task

--- Year-11-Term-1/students.py
+++ Year-11-Term-1/students.py
@@ -12,53 +12,73 @@
 
 # display list
 
+import re
+
 nameList = []
 ageList = []
 emailList = []
-counter = 0
+NUMBER_OF_STUDENTS = int(input("Number of students: "))
 
 def get_name():
-  for i in range(5):
+  i = 0
+
+  while i < NUMBER_OF_STUDENTS:
     name = input(f"Name {i+1}: ")
+
     while validate_name(name) == False:
       print("Invalid name")
-      counter -= 1
       break
-    nameList.append(name)
+    
+    if validate_name(name):
+      i += 1
+      nameList.append(name)
 
 def validate_name(name):
-  if name == "":
+  if name.strip() == "":
     return False
   else:
     return True
 
 def get_age():
-  for i in range(5):
-    age = int(input(f"Age {i+1}: "))
+  i = 0
+
+  while i < NUMBER_OF_STUDENTS:
+    age = input(f"Age {i+1}: ")
+
     while validate_age(age) == False:
       print("Invalid age")
-      i -= 1
       break
-    ageList.append(age)
+    
+    if validate_age(age):
+      i += 1
+      ageList.append(age)
 
 def validate_age(age):
-  if age < 8 or age > 80:
-    return False
+  if age.isnumeric():
+    if (8 < int(age) < 80):
+      return True
+    else:
+      return False
   else:
-    return True
-
+    return False
 
 def get_email():
-  for i in range(5):
+  i = 0
+
+  while i < NUMBER_OF_STUDENTS:
     email = input(f"Email {i+1}: ")
+
     while validate_email(email) == False:
       print("Invalid email")
-      i -= 1
       break
-    emailList.append(email)
+
+    if validate_email(email):
+      i += 1
+      emailList.append(email)
 
 def validate_email(email):
-  if "@" in email:
+  regex = r"^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$"
+  if re.search(regex, email):
     return True
   else:
     return False
@@ -67,6 +87,6 @@ def main():
   get_name()
   get_age()
   get_email()
-  print(nameList, ageList, emailList)
+  print(nameList, '\n', ageList, '\n', emailList)
 
 main()
