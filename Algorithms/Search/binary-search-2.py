5e5aac702d9314cd7d6fa4f4606ce1654a0895f7
azure8117 <abdulahad2004@yahoo.com>
2021-02-21 18:46:49 +0300

    Binary search fixed

--- Algorithms/Search/binary-search-2.py
+++ Algorithms/Search/binary-search-2.py
@@ -1,5 +1,3 @@
-import math
-
 data_set = [3, 9, 13, 15, 21, 24, 27, 30, 36, 39, 42, 54, 69]
 first = 0
 last = len(data_set) - 1
@@ -7,17 +5,17 @@ found = False
 target = int(input("Number to search: "))
 
 while found == False and first <= last:
-    middle = math.ceil((first + last) / 2)
+    middle = int((first + last) / 2)
 
     if data_set[middle] == target:
         found = True
-        print(f"{target} is found.")
+        print(f"{target} is found at index {middle}")
     
     elif target > data_set[middle]:
-        first = middle - 1
+        first = middle + 1
     
     elif target < data_set[middle]:
-        last = middle + 1
+        last = middle - 1
     
 if found == False:
     print("Not found.")
